06946ba94652b94720654653f20c55175ea00e8f
PrivateHistory <davidrotari9@gmail.com>
2020-09-29 15:41:12 +0200

    Facut tot

--- Bot.py
+++ Bot.py
@@ -1,34 +1,154 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler,Updater
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import LoginUrl
+import os
+import sys
 import pyqiwi
-current_items=[]
-wallet = pyqiwi.Wallet(token='f372cb25954fb4f8bb6b5051f026d7e8')
-updater=Updater(token="1396381300:AAFtNnWv2adnSg7L3ZZQRr6x_ZNvGk39zG0",use_context=True)
-dispatcher = updater.dispatcher
+### function
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to online shop,I am your bot and I will help you!")
-    context.bot.send_message(chat_id=update.effective_chat.id,text="Bellow are a list of command you can use")
-    context.bot.send_message(chat_id=update.effective_chat.id,text="/items  list the items in the shop")
-    context.bot.send_message(chat_id=update.effective_chat.id,text="/current_items list of current items you bought")
-    context.bot.send_message(chat_id=update.effective_chat.id,text="/buy+item buy and item from a list,dont forget to include the item too")
-    context.bot.send_message(chat_id=update.effective_chat.id,text="/show_money show how much money I have on account")
-def buy(update,conext,user_data):
-    context.bot.send_message(chat_id=update.effective.chat.id,text="you want to buy "+str(user_data))
-def items(update,context):
-    items=["trousers","hat","glasses"]
-    context.bot.send_message(chat_id=update.effective_chat.id,text=",".join(items))
-def current_items(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id,text=",".join(current_items))
-def show_money(update,context):
-    context.bot.send_message(chat_id=update.effective_chat.id,text=str(wallet.balance()))
-start_handler = CommandHandler('start', start)
-items_handler=CommandHandler("items",items)
-current_items_handler=CommandHandler("current_items",items)
-buy_handler=CommandHandler("buy",buy,pass_user_data=True)
-show_money_handler=CommandHandler("show_money",show_money)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(items_handler)
-dispatcher.add_handler(current_items_handler)
-dispatcher.add_handler(show_money_handler)
+  update.message.reply_text("Добро пожаловать "+str(update.message.from_user.id) +" в Les Miserable онлайн магазин",
+                            reply_markup=main_menu_keyboard())
+
+def main_menu(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text="Добро пожаловать обратно в магазин Les Miserable",
+                        reply_markup=main_menu_keyboard())
+
+def shop(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text=shop_message(),
+                        reply_markup=shop_keyboard())
+
+def wallet(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(
+                        text=wallet_message(),
+                        reply_markup=wallet_keyboard())
+
+def support(update,context):
+   pass
+
+
+def shop_Uplay(update,context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+                          text="Стоимость Uplay 500 руб. Хотите купить?",
+                          reply_markup=Uplay_keyboard())
+
+def shop_Lol(update,context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+                          text="Стоимость Lol 550 руб. Хотите купить?",
+                          reply_markup=Uplay_keyboard())
+
+def shop_Origin(update,context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+                          text="Стоимость Origin 700 руб. Хотите купить?",
+                          reply_markup=Origin_keyboard())
+
+def shop_Check_Object(update,context):
+        query = update.callback_query
+        query.answer()
+        query.edit_message_text(
+                              text="Стоимость Check Object 1 руб. Хотите купить?",
+                              reply_markup=Check_Object_keyboard())
+
+def wallet_balance(update,context):
+    pass
+
+
+def wallet_put_money(update,context):
+    pass
+
+def Uplay_buy(update,context):
+    pass
+def Check_Object_buy(update,context):
+    pass
+def Origin_buy(update,context):
+    pass
+def Lol_buy(update,context):
+    pass
+
+####keyboards####
+def main_menu_keyboard():
+    keyboard=[[InlineKeyboardButton("Mагазин",callback_data="shop")],
+    [InlineKeyboardButton("Kошелек",callback_data="wallet")],
+    [InlineKeyboardButton("Суппорт",callback_data="support")]]
+    return InlineKeyboardMarkup(keyboard)
+
+def shop_keyboard():
+    keyboard=[[InlineKeyboardButton("Uplay",callback_data="Uplay_Shop")],
+    [InlineKeyboardButton("Origin",callback_data="Origin_Shop")],
+    [InlineKeyboardButton("Lol",callback_data="Lol_Shop")],
+    [InlineKeyboardButton("Check Object",callback_data="Check_Object_Shop")],
+    [InlineKeyboardButton("Главное",callback_data="main")]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def wallet_keyboard():
+    keyboard=[[InlineKeyboardButton("Баланс",callback_data="wallet_balance")],
+    [InlineKeyboardButton("Добавить",callback_data="wallet_put_money")],
+    [InlineKeyboardButton("Главное",callback_data="main")]]
+    return InlineKeyboardMarkup(keyboard)
+
+def Uplay_keyboard():
+    keyboard=[[InlineKeyboardButton("Купить",callback_data="Buy_Uplay")],
+    [InlineKeyboardButton("Главное",callback_data="main")]]
+    return InlineKeyboardMarkup(keyboard)
+
+def Origin_keyboard():
+    keyboard=[[InlineKeyboardButton("Купить",callback_data="Buy_Origin")],
+    [InlineKeyboardButton("Главное",callback_data="main")]]
+    return InlineKeyboardMarkup(keyboard)
+
+def Lol_keyboard():
+        keyboard=[[InlineKeyboardButton("Купить",callback_data="Buy_Lol")],
+        [InlineKeyboardButton("Главное",callback_data="main")]]
+        return InlineKeyboardMarkup(keyboard)
+
+def Check_Object_keyboard():
+    keyboard=[[InlineKeyboardButton("Купить",callback_data="Buy_Check_Object")],
+    [InlineKeyboardButton("Главное",callback_data="main")]]
+    return InlineKeyboardMarkup(keyboard)
+
+###mesages
+def shop_message():
+    return "Mагазин"
+
+def wallet_message():
+    return "Kошелек"
+
+def stop():
+    print("It wroked")
+    updater.stop()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+updater=Updater(token="1182408515:AAH8DTTiYWtY7wfqSTYgjc7WSUI87oamPrE",use_context=True)
+
+##dispatcher
+updater.dispatcher.add_handler(CommandHandler('stop',stop))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu,pattern="main"))
+updater.dispatcher.add_handler(CallbackQueryHandler(shop,pattern="shop"))
+updater.dispatcher.add_handler(CallbackQueryHandler(wallet,pattern="wallet"))
+updater.dispatcher.add_handler(CallbackQueryHandler(Check_Object_buy,pattern="Buy_Check_Object"))
+updater.dispatcher.add_handler(CallbackQueryHandler(Lol_buy,pattern="Buy_Lol"))
+updater.dispatcher.add_handler(CallbackQueryHandler(Origin_buy,pattern="Buy_Origin"))
+updater.dispatcher.add_handler(CallbackQueryHandler(Uplay_buy,pattern="Buy_Uplay"))
+updater.dispatcher.add_handler(CallbackQueryHandler(wallet_put_money,pattern="wallet_put_money"))
+updater.dispatcher.add_handler(CallbackQueryHandler(wallet_balance,pattern="wallet_balance"))
+updater.dispatcher.add_handler(CallbackQueryHandler(shop_Uplay,pattern="Uplay_Shop"))
+updater.dispatcher.add_handler(CallbackQueryHandler(shop_Lol,pattern="Lol_Shop"))
+updater.dispatcher.add_handler(CallbackQueryHandler(shop_Origin,pattern="Origin_Shop"))
+updater.dispatcher.add_handler(CallbackQueryHandler(shop_Check_Object,pattern="Check_Object_Shop"))
+updater.dispatcher.add_handler(CallbackQueryHandler(support,pattern="support"))
+
 updater.start_polling()
-updater.idle()
